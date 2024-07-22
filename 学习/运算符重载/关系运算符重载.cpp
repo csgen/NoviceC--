794845cef274c577f24f8d5eba309d3c9b92a2dd
csgen <csgen@umich.edu>
2024-07-23 00:15:47 +0800

    仿函数

--- 学习/运算符重载/关系运算符重载.cpp
+++ 学习/运算符重载/关系运算符重载.cpp
@@ -16,11 +16,18 @@ public:
 		}
 		return false;
 	}
+
+	bool operator!=(Person& p) {
+		if (this->m_Name == p.m_Name && this->m_Age == p.m_Age) {
+			return false;
+		}
+		return true;
+	}
 };
 
 void test05() {
 	Person p1("Tom", 18);
-	Person p2("Tom", 18);
+	Person p2("Jerry", 18);
 	if (p1 == p2) {
 		cout << "p1 equals to p2" << endl;
 	}
@@ -29,8 +36,8 @@ void test05() {
 	}
 }
 
-int main() {
-	test05();
-	system("pause");
-	return 0;
-}
+//int main() {
+//	test05();
+//	system("pause");
+//	return 0;
+//}
---
+++ 学习/运算符重载/函数调用运算符重载.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class MyPrint {
+public:
+	void operator()(string text) {
+		cout << text << endl;
+	}
+};
+void test06() {
+	MyPrint myPrint;
+	myPrint("HELLO");// 重载()操作符，又称仿函数
+}
+
+int main() {
+	test06();
+
+	// 匿名对象调用
+	MyPrint()("HI");
+
+	system("pause");
+	return 0;
+}
